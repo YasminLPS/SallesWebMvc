077cc9fb07941cf67d20da5ce228cc8e0ffbd393
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-08-23 16:56:31 -0300

    Update seller and custom service exception

--- SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SallesWebMvc.Models;
 using SallesWebMvc.Models.ViewsModels;
 using SallesWebMvc.Services;
+using SallesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -81,6 +82,47 @@ namespace SallesWebMvc.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException) 
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ SallesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SallesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) 
+            :base(message)
+        {
+          
+        }
+    }
+}
