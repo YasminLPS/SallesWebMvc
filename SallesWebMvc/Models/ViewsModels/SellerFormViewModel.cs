d739715504bf18fcf6a84ff1bbf2a42182d06567
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-08-23 15:43:33 -0300

    SellerFormViewModel and Department select component

---
+++ SallesWebMvc/Models/ViewsModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SallesWebMvc.Models.ViewsModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
--- SallesWebMvc/Services/DepartmentService.cs
+++ SallesWebMvc/Services/DepartmentService.cs
@@ -6,25 +6,18 @@ using System.Threading.Tasks;
 
 namespace SallesWebMvc.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
         private readonly SallesWebMvcContext _context;
 
-        public SellerService(SallesWebMvcContext context)
+        public DepartmentService(SallesWebMvcContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll() 
+        public List<Department> FindAll() 
         {
-            return _context.Seller.ToList();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
-
-        public void Insert(Seller obj) 
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
-
     }
 }
