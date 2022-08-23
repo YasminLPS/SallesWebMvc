0ece49d459a9153c65ffecbd71912f9eca97e697
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-08-22 21:25:42 -0300

    Other entities and second migration

--- SallesWebMvc/Models/Department.cs
+++ SallesWebMvc/Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SallesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SallesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name, ICollection<Seller> sellers)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSellers(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) 
+        {
+            return Sellers.Sum(sl => sl.TotalSales(initial, final));
+                          // para cada vendedo (sl) na lista, chame o TotalSales(metodo criado) no periodo (initial/final)
+                                  //  e soma (Sum) desse resultado para todos os vendedores do meu departamento 
+        }
     }
 }
